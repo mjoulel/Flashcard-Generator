182d2a92c58d82bf0114adfce7fbdcdac68b4db3
mjoulel <joulelm@gmail.com>
2017-01-26 00:50:20 -0600

    Add files via upload

---
+++ flashcard.js
@@ -0,0 +1,12 @@
+// Marie Laborde
+// 1/26/2017
+// flashcard homework
+
+// Create a BasicCard constructor. It should accept front and back arguments.
+function BasicCard(front, back){};
+
+// Create a ClozeCard constructor. It should accept text and cloze arguments.
+function ClozeCard(text, cloze){
+	// property or method that contains or returns only the cloze-deleted portion of the text.
+	this.cloze-deleted = function(){};
+};
